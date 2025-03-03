b8492e16ae0ab16838d22377a624a0313f3db72d
dylant313 <dylant313@yahoo.com>
2025-03-03 11:49:40 -0500

    Use firebase service account file instead of default credentials

--- livelearn-backend/functions/src/config/firebaseConfig.ts
+++ livelearn-backend/functions/src/config/firebaseConfig.ts
@@ -1,6 +1,8 @@
-import * as admin from 'firebase-admin'
+import * as admin from "firebase-admin";
 import { getFirestore } from "firebase-admin/firestore";
 
-admin.initializeApp();
+admin.initializeApp({
+  credential: admin.credential.cert("src/config/livelearn-fe28b-firebase-adminsdk-fbsvc-325f5608b2.json")
+});
 
 export const db = getFirestore();
