d488a58a9252acae659efe6e4f9125d4bd6c712f
vedansh0628 <vedansh0628@gmail.com>
2025-04-01 05:01:32 -0400

    comment unused imports

--- livelearn-frontend/src/main.tsx
+++ livelearn-frontend/src/main.tsx
@@ -16,9 +16,9 @@ import FRQResult from "./Student-Screens/FRQResult.tsx";
 import Edit from "./Teacher-Screens/Edit.tsx";
 import Session from "./Teacher-Screens/Session.tsx";
 import FRQFeedback from "./Student-Screens/FRQFeedback";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
-import firebaseApp from './firebaseConfig'; // Make sure to set up this file
-import { db } from "../firebase";
+// import { getFirestore, collection, getDocs } from "firebase/firestore";
+// import firebaseApp from './firebaseConfig'; // Make sure to set up this file
+// import { db } from "../firebase";
 
 import "./index.css";
 
